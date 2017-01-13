73d68ac29f93fc18c12f7c174c7fcaf9ecca8d39
stormy seliquini <stormyseliquini3of3@gmail.com>
2017-01-13 09:46:24 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// provide jquery initian function
+$(document).ready(function() {
+
+    var planets = [
+        ['Pluto', 0.06],
+        ['Neptune', 1.148],
+        ['Uranus', 0.917],
+        ['Saturn', 1.139],
+        ['Jupiter', 2.640],
+        ['Mars', 0.3895],
+        ['Moon', 0.1655],
+        ['Earth', 1],
+        ['Venus', 0.9032],
+        ['Mercury', 0.377],
+        ['Sun', 27.9]
+    ];
+    /* cycle through the array and get the [1] value for chosen planet (which is the gravity)
+     *************
+     *************
+     */
+    $.each(planets, function(index, value) {
+        $('#selectPlanet').append(`<option value = '${value[1]}'> ${ value[0] } </option>`);
+
+    });
+    /* write calculation and make it on-click
+     *************
+     *************
+     */
+    $("button").on("click", calcuLate);
+
+    function calcuLate() {
+
+        // use jquery $ function instead of .getElementById to call the textbox from the html
+        var enterWeight = $("#inputWeight").val();
+
+        // reference the planet that is selected to its gravity
+        var psGravity = selectPlanet.options[selectPlanet.selectedIndex].value;
+
+
+        // multiply the referenced planet gravity with the input weight push it to the html file with jquery method
+        var outPut = psGravity * enterWeight;
+        var newOutPut = Math.round(outPut * 100) / 100;
+        $("#outPut").text(newOutPut);
+    }
+});
